c94ff3b0abdf332a682bf6c578657cb5afb2ed34
Sajid-ul Islam <saajiidi@gmail.com>
2026-04-03 11:32:01 +0600

    .

--- tmp/check_sheets.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-source = r"g:\Github\Automation Pivot\data\22_25.xlsx"
-xlsx = pd.ExcelFile(source)
-print("Sheet Names:", xlsx.sheet_names)
--- tmp/check_sheets_fast.py
+++
@@ -1,4 +0,0 @@
-import openpyxl
-source = r"g:\Github\Automation Pivot\data\22_25.xlsx"
-wb = openpyxl.load_workbook(source, read_only=True)
-print("Sheet Names:", wb.sheetnames)
